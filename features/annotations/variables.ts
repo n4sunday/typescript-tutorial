4383b1f7de900214ac9b5d4e6526a316a4f26e33
Sunday <s5935512004@phuket.psu.ac.th>
2020-01-30 05:34:50 +0700

    :bulb: Lesson 20 Delayed Initialization

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -36,5 +36,14 @@ const json = '{"x": 10,"y": 20}'
 const coordinates: { x: number; y: number } = JSON.parse(json)
 console.log(coordinates)
 
+// 2) When we declare a variable on one line
+// and initalzate it later
+let words = ['red', 'green', 'blue']
+let foundWord: boolean
 
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true
+    }
+}
 
