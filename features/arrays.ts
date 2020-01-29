d72751195bb12a833b9b7542dff59fdc270d5167
Sunday <s5935512004@phuket.psu.ac.th>
2020-01-30 06:18:04 +0700

    :bulb: Lesson 28 Arrays in Typescript

---
+++ features/arrays.ts
@@ -0,0 +1,14 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy']
+console.log('carMakers: ', carMakers)
+
+const dates = [new Date(), new Date()]
+
+const carsByMake: string[][] = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+]
+
+console.log('carsByMake: ', carsByMake)
+
+
