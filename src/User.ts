8536c0b717c9bab25bc67692c46d43ec63ad43f3
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-01 04:25:30 +0700

    :bulb: Lesson 55 Defining a Company

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,6 @@
 import faker from 'faker'
 
-class User {
+export class User {
     name: string
     location: {
         lat: number
@@ -9,7 +9,6 @@ class User {
 
     constructor() {
         this.name = faker.name.firstName()
-        console.log(this.location)
         this.location = {
             lat: parseFloat(faker.address.latitude()),
             lng: parseFloat(faker.address.longitude()),
