f3a3316c5090e17d75ac9908c87aa7ad51bdcaeb
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-01 03:07:56 +0700

    :bulb: Lesson 44 Instance Methods Modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,9 +1,9 @@
 class Vehicle {
-    drive(): void {
+    public drive(): void {
         console.log('chugga chugga')
     }
 
-    honk(): void {
+    public honk(): void {
         console.log('beep')
     }
 }
@@ -12,12 +12,15 @@ class Car extends Vehicle {
     drive(): void {
         console.log('room')
     }
+
+    startDrivingProcess(): void {
+        this.drive()
+        this.honk()
+    }
 }
 
 const vehicle = new Vehicle()
 vehicle.drive()
-vehicle.honk()
 
 const car = new Car()
-vehicle.drive()
-vehicle.honk()
+car.startDrivingProcess()
